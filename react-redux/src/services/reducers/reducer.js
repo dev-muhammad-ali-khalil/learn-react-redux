005c7340c73bcc1bb49d4597853938c5455432e0
Muhammad Ali Khalil <dev.alikhalil@gmail.com>
2025-04-07 19:53:42 +0500

    Reducer & Root Reducer

---
+++ react-redux/src/services/reducers/reducer.js
@@ -0,0 +1,19 @@
+import { add_to_cart, remove_from_cart } from "../constants"
+
+const initialState = {
+    cardData: []
+}
+
+const cardItems = (state = initialState, action) => {
+    switch (action.type) {
+        case add_to_cart:
+            return {
+                ...state, cardData: action.data
+            }
+        default:
+            return state
+
+    }
+}
+
+export default cardItems
